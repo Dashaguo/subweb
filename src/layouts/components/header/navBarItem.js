1c4a497e4e17c6b1194c522cbd99911344fcc075
Dashaguo <33348166+Dashaguo@users.noreply.github.com>
2023-07-08 14:46:38 +0800

    Update navBarItem.js

--- src/layouts/components/header/navBarItem.js
+++ src/layouts/components/header/navBarItem.js
@@ -1,17 +1,7 @@
 export const navBarItem = [
   {
-    title: '首页',
-    link: '/',
-    target: '',
-  },
-  {
     title: '短链接',
     link: 'https://opi.lol',
-    target: '_blank',
-  },
-  {
-    title: 'GitHub',
-    link: 'https://github.com/stilleshan/subweb',
     target: '_blank',
   },
 ];
